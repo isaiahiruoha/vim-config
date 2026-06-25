c92d5477dc04fed6c3672d7f1a67ff18d08fe2a9
Isaiah Iruoha <76797894+isaiahiruoha@users.noreply.github.com>
2026-06-25 15:46:45 -0400

    adjusted my wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,32 +1,32 @@
--- Pull in the wezterm API
+
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+local is_windows = os.getenv("OS") and os.getenv("OS"):lower():find("windows")
+local is_macos = wezterm.target_triple:lower():find("darwin") ~= nil
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 15
-
+config.color_scheme = "rose-pine-moon"
+config.max_fps = 120
+config.font = wezterm.font("Hack Nerd Font", { weight = "DemiBold" })
 config.enable_tab_bar = false
-
 config.window_decorations = "RESIZE"
-
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
-
-config.colors = {
-        foreground = "#CBE0F0",
-        background = "#011423", -- 011423
-        cursor_bg = "#47FF9C",
-        cursor_border = "#47FF9C",
-        cursor_fg = "#011423",
-        selection_bg = "#033259",
-        selection_fg = "#CBE0F0",
-        ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-        brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+config.inactive_pane_hsb = {
+  saturation = 0.0,
+  brightness = 0.5,
 }
 
--- and finally, return the configuration to wezterm
+if is_windows then
+  config.win32_system_backdrop = "Acrylic"
+  config.window_background_opacity = 0.7
+  config.window_frame = { font_size = 10.0 }
+end
+
+if is_macos then
+  config.window_background_opacity = 0.8
+  config.macos_window_background_blur = 50
+  config.font_size = 15.0
+  config.window_frame = { font_size = 13.0 }
+end
+
 return config
